331049ce62b44001df6cc66002d54ef062a0772b
Sixmore <i@ajiangshan.cn>
2014-10-18 15:13:38 +0800

    Create cachematrix.R

---
+++ cachematrix.R
@@ -0,0 +1,15 @@
+## Solve x
+
+## Solve x
+
+makeCacheMatrix <- function(x = matrix()) {
+        x
+}
+
+
+## Solve x
+
+cacheSolve <- function(x, ...) {
+        y <- solve(makeCacheMatrix(x)) ## y is the inverse of x
+        y  ## return y
+}
